492042c7491329e49bddddd2febf39cb5806af19
kyu09 <guiheng0806@gmail.com>
2022-10-18 17:58:44 +0900

    .

---
+++ children/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
+
+		navigation: true, /* 오른쪽에 각 페이지의 paging */
+		navigationPosition: 'right', /* 위치 */
+		navigationTooltips: ['메인', '두번째', '세번째', '네번째'], /* 툴팁 */
+		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+
+		
+
+		responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+	});
+});
