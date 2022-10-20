0684ecb1490e0ea3220e0548702430da84b11676
kyu09 <guiheng0806@gmail.com>
2022-10-20 12:52:18 +0900

    .

--- children/js/main.js
+++ children/js/main.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
+
+    // const myFullpage = new fullpage('#fullpage', {});  이렇게만 써도 fullpage는 기본 동작함
+
     const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
 		navigation: true, /* 오른쪽에 각 페이지의 paging */
 		navigationPosition: 'right', /* 위치 */
-		navigationTooltips: ['메인', '두번째', '세번째', '네번째'], /* 툴팁 */
-		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+		navigationTooltips: ['메인', '보건통계', '사업소개', '현장소식', '지원사업'], /* 툴팁 */
+		showActiveTooltip: false, /* 현재 활성화된 페이지의 툴팁 보이기 */
 
 		afterLoad: function(origin, destination, direction, trigger){
-			// console.log(destination.index);      
+			//console.log(destination.index);
 			if((destination.index == 2)||(destination.index == 3)||(destination.index == 4)||(destination.index == 5)){
 				$('.header').addClass('black');
 				$('#fp-nav').addClass('black');
@@ -15,14 +18,26 @@ $(document).ready(function(){
 				$('.header').removeClass('black');
 				$('#fp-nav').removeClass('black');
 			}
+			if(destination.index == 1){ /* 보건통계 슬라이드가 나타났을때 */
+				funCount();  // 함수 이름이 funCount인 것을 실행
+			}
 		},
-		 
 		responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
-	});
+	});//fullpage
 
+	//count 숫자 count-up
+	// $('.count .rate li .num strong').counterUp({
+	// 	delay: 10,   /* 애니메이션이 작동되기 이전 대기 시간 */
+	// 	time: 2000    /* 전체 애니메이션 시간 */
+	// });//countup
 
-	$('.count .rate li .num strong').counterUp({
-		delay : 10,
-		time : 2000,
-	});
-});
+	/* 보건통계 숫자 카운팅 효과 */
+	function funCount(){
+		console.log('카운팅동작');
+		$('.count .rate li .num strong').counterUp({
+			time: 1000    /* 전체 애니메이션 시간 */
+		});
+	}
+	
+
+});//document.ready
