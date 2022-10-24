ecd4a2384c6778de285d32faf4e6322fa9fac664
kyu09 <guiheng0806@gmail.com>
2022-10-24 17:59:58 +0900

    .

--- children/js/main.js
+++ children/js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
 		navigationPosition: 'right', /* 위치 */
 		navigationTooltips: ['메인', '보건통계', '사업소개', '현장소식', '지원사업'], /* 툴팁 */
 		showActiveTooltip: false, /* 현재 활성화된 페이지의 툴팁 보이기 */
+		
+		// scrollOverflow: false, 
 
 		afterLoad: function(origin, destination, direction, trigger){
 			//console.log(destination.index);
@@ -18,18 +20,12 @@ $(document).ready(function(){
 				$('.header').removeClass('black');
 				$('#fp-nav').removeClass('black');
 			}
-			if(destination.index == 1){ /* 보건통계 슬라이드가 나타났을때 */
-				funCount();  // 함수 이름이 funCount인 것을 실행
+			if(destination.index == 1){ //보건통계 슬라이드가 나타났을때
+				funCount();//함수 이름이 funCount인 것을 실행
 			}
 		},
 		responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
 	});//fullpage
-
-	//count 숫자 count-up
-	// $('.count .rate li .num strong').counterUp({
-	// 	delay: 10,   /* 애니메이션이 작동되기 이전 대기 시간 */
-	// 	time: 2000    /* 전체 애니메이션 시간 */
-	// });//countup
 
 	/* 보건통계 숫자 카운팅 효과 */
 	function funCount(){
@@ -38,32 +34,29 @@ $(document).ready(function(){
 			time: 1000    /* 전체 애니메이션 시간 */
 		});
 	}
-	
+
+	/* 최근소식에 swiper효과 */
 	const swiper = new Swiper('.news .list', { /* 팝업을 감싼는 요소의 class명 */
-		slidesPerView: 1, /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
-		spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
+		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
 		breakpoints: {
-			450: {  /* 640px 이상이 되면 적용 */
+			450: {    /* 640px 이상일때 적용 */
 				slidesPerView: 2,
-				spaceBetween: 20, 
+				spaceBetween: 20,
 			},
-			800: {  /* 640px 이상이 되면 적용 */
-			slidesPerView: 3,
-			spaceBetween: 30, 
+			800: {    /* 640px 이상일때 적용 */
+				slidesPerView: 3,
+				spaceBetween: 30,
 			},
-			1200: {  /* 640px 이상이 되면 적용 */
-			slidesPerView: 4,
-			spaceBetween: 40, 
+			1200: {    /* 1200px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 40,
 			},
 		},
 		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-			el: '.swiper-pagination', /* 해당 요소의 class명 */
+			el: '.btn_paging', /* 해당 요소의 class명 */
 			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
 		},
 	});
-
-
-
-
 
 });//document.ready
