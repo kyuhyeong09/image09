f91d2fb774cd50a71aa0b70f6ac83abcefb5282a
kyu09 <guiheng0806@gmail.com>
2022-10-27 18:00:16 +0900

    .

--- bntailor/js/main.js
+++ bntailor/js/main.js
@@ -14,7 +14,31 @@ $(document).ready(function(){
             el: '.btn_paging', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
         },
-
-
     });
+    $('.visual .popup .btn_stop').on('click', function(){
+        let popStatus = $(this).hasClass('play');
+        if(popStatus == true){
+            swiper.autoplay.start();
+            $(this).removeClass('play');
+            $(this).text('일시정지');
+        }else{
+            swiper.autoplay.stop();
+            $(this).addClass('play');
+            $(this).text('재생');
+        }
+    });
+
+
+    let scrolling;
+    fabScroll();
+    $(window).scroll(function(){
+       fabScroll();
+    });
+
+    function fabScroll(){
+        scrolling = $(window).scrollTop();
+        console.log(scrolling);
+        moveTop = scrolling*0.15;
+        $('.fabric .bg img').css('transform','translate(0, -'+moveTop+'px)');
+    }
 });
